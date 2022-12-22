3c08f31eda5061086d571a87c886fa868bfe5e2c
kartiksrinivas007 <kartiksrinivas008@gmail.com>
2022-12-22 14:10:25 +0530

    Maimn Test added

---
+++ main.py
@@ -0,0 +1,10 @@
+import torch
+import numpy as np
+import Utility as ut
+import sys
+
+
+if __name__ == "__main__":
+    # print(sys.path)
+    ut.test(3, 1, 160, 160)
+    print("Yes the sizes matched!")
